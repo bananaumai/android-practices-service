f4fa0c29e4112f8a7e0380a701052592a2c24a08
Yuta Shimakawa <bananaumai@tinkermode.com>
2019-04-30 23:56:51 +0900

    delay simulation

--- bound/src/main/java/dev/bananaumai/bound/Service.kt
+++ bound/src/main/java/dev/bananaumai/bound/Service.kt
@@ -1,5 +1,6 @@
 package dev.bananaumai.bound
 
+import android.app.IntentService
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -7,9 +8,67 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.os.Binder
-import android.os.IBinder
+import android.os.*
 import android.util.Log
+import java.io.Serializable
+import kotlin.random.Random
+
+class DataProcessor : Service() {
+    private val tag = this.javaClass.name
+    private lateinit var serviceHandler: ServiceHandler
+    private val rand = Random.Default
+
+    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
+        override fun handleMessage(msg: Message?) {
+            val intent = msg?.obj as Intent?
+
+            val i = rand.nextInt(10)
+
+            if (i % 4 == 0) {
+                Thread.sleep(10)
+            }
+
+            if (intent != null) {
+                val data = intent.getStringExtra("data")
+                Log.d(tag, data)
+            }
+
+            Log.d(tag, "isIdle?: ${looper.queue.isIdle}")
+
+            val printer = { x: String? ->
+                Log.d(tag, x)
+                Unit
+            }
+
+            if (!looper.queue.isIdle) {
+                looper.dump(printer, "hoge")
+            }
+
+            super.handleMessage(msg)
+        }
+    }
+
+    override fun onCreate() {
+        HandlerThread("DataProcessorHandlerThread", Process.THREAD_PRIORITY_BACKGROUND).apply {
+            start()
+            serviceHandler = ServiceHandler(looper)
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        serviceHandler.obtainMessage().also { msg ->
+            msg.arg1 = startId
+            msg.obj = intent
+            serviceHandler.sendMessage(msg)
+        }
+
+        return START_STICKY
+    }
+}
 
 class DataEmitter : Service() {
     private val tag = this.javaClass.name
@@ -25,13 +84,16 @@ class DataEmitter : Service() {
 
         super.onCreate()
 
-        val logger = { data: Any ->
-            Log.d(tag, data.toString())
+        val dispatcher = { data: Any ->
+            Intent(this, DataProcessor::class.java).also {
+                it.putExtra("data", data.toString())
+                startService(it)
+            }
             Unit
         }
 
         val accelerometer = Accelerometer(getSystemService(Context.SENSOR_SERVICE) as SensorManager)
-        accelerometer.listen(logger)
+        accelerometer.listen(dispatcher)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -51,7 +113,7 @@ interface EventListener {
 }
 
 class Accelerometer(private val manager: SensorManager) : EventListener {
-    inner class AccelerometerEvent(private val event: SensorEvent) {
+    inner class AccelerometerEvent(private val event: SensorEvent) : Serializable {
         override fun toString() =
             "${this.javaClass.name} { timestamp: ${event.timestamp}, values: ${event.values.joinToString(",", "[", "]")} }"
     }
@@ -70,6 +132,6 @@ class Accelerometer(private val manager: SensorManager) : EventListener {
             override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
             }
         }
-        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME)
+        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
